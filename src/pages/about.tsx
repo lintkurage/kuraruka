7ee22e242e2ccee3a4e44dc75f631b8caebe5a6d
lintkurage <lintkurage@gmail.com>
2024-10-26 14:55:20 +0900

    レーダーチャートを作成する

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,6 +10,7 @@ export default function About() {
     <>
     <Layout>
         <Abouthero></Abouthero>
+
     </Layout>
     </>
   );
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -9,7 +9,6 @@ export default function Blog() {
   return (
     <>
     <Layout title="Blogページです">
-      <p>このページはブログのページです.</p>
     </Layout>
     </>
   )
