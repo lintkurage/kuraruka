b2407e9cc55148df30257fbe2157d3426187ab80
lintkurage <lintkurage@gmail.com>
2024-10-26 14:13:49 +0900

    skillsetコンポーネント作成

---
+++ src/components/skillset.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Skillset(){
+    return(
+        <>
+        </>
+    );    
+}
