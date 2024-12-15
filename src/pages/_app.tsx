a723905f473f6204cd5549b6aee83c7d889c5a98
lintkurage <lintkurage@gmail.com>
2024-12-16 00:19:07 +0900

    Google Analytics の導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script';
+import * as gtag from 'src/libs/gtag'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return <>
   <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_ID}`}
       />
       <Script
         id="gtag-init"
@@ -16,7 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            gtag('config', '${gtag.GA_ID}');
           `,
         }}
       />
